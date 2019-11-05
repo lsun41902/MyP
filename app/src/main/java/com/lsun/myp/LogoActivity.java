87924953de4e278d4049574da638365ccceebda0
lsun41902 <blossom41902@gmail.com>
2019-11-05 17:34:13 +0900

    check point 11/05 start Loading

--- app/src/main/java/com/lsun/myp/LogoActivity.java
+++ app/src/main/java/com/lsun/myp/LogoActivity.java
@@ -4,19 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
 
 public class LogoActivity extends AppCompatActivity {
+    private Handler mHandler;
+    private Runnable mRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        startActivity(new Intent(this,MainActivity.class));
-        finish();
+
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LogoActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        mHandler = new Handler();
+        mHandler.postDelayed(mRunnable, 3000);
+
     }
+
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.spin_kit);
+        Sprite doubleBounce = new DoubleBounce();
+        progressBar.setIndeterminateDrawable(doubleBounce);
+    }
+    @Override
+    protected void onDestroy() {
+        mHandler.removeCallbacks(mRunnable);
+        super.onDestroy();
+    }
+
+
 }
